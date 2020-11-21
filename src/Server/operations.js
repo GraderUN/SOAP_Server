6d8d8a1d58121ab72a6c190ebdd534d1995925bc
saduquebe <saduquebe@unal.edu.co>
2020-11-21 00:47:27 -0500

    minor bug fixes

--- src/Server/operations.js
+++ src/Server/operations.js
@@ -1,8 +1,24 @@
+const client = require('./request_ms');
+
 async function prueba(args) {
     return "hola";
 }
-async function getStudents(){
-    return "funcion correcta";
+async function getStudents(args){
+    /*client.query({
+        query: gql`
+          query GetRates {
+            rates(currency: "USD") {
+              currency
+            }
+          }
+        `
+      }).then(
+        result =>{
+            console.log(result);
+            return result;
+        }
+        );*/
+        return "correcto";
 }
 module.exports = { 
     prueba,
--- src/Server/request_ms.js
+++ src/Server/request_ms.js
@@ -1,6 +1,18 @@
 const fetch = require('node-fetch');
-
+//import ApolloClient from 'apollo-boost';
+//import { ApolloClient, InMemoryCache } from '@apollo/client';
+/*
+const client = new ApolloClient({
+  uri: 'https://48p1r2roz4.sse.codesandbox.io',
+  cache: new InMemoryCache()
+});
+*/
 //Definiendo la URL
 const serverData = require('../server');
 let baseUrlFeedMs = `http://${serverData.feed_url}/${serverData.feed_entryPoint}`;
 let baseUrlLabelsMs = `http://${serverData.labels_url}/${serverData.labels_entryPoint}`;
+/*
+module.exports = {
+    client
+}
+*/

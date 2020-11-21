a8335cd89ca840a59dd19ccb615b6bf5bc639ff3
saduquebe <saduquebe@unal.edu.co>
2020-11-21 15:10:14 -0500

    Functional Students SOAP request :construction_worker:

--- src/Server/request_ms.js
+++ src/Server/request_ms.js
@@ -1,18 +1,19 @@
-const fetch = require('node-fetch');
-//import ApolloClient from 'apollo-boost';
-//import { ApolloClient, InMemoryCache } from '@apollo/client';
-/*
-const client = new ApolloClient({
+const gql = require("graphql-tag");
+const ApolloClient = require("apollo-client").ApolloClient;
+const fetch = require("node-fetch");
+const createHttpLink = require("apollo-link-http").createHttpLink;
+const InMemoryCache = require("apollo-cache-inmemory").InMemoryCache;
+
+const httpLink = createHttpLink({
   uri: 'https://48p1r2roz4.sse.codesandbox.io',
+  fetch: fetch
+});
+
+const client = new ApolloClient({
+  link: httpLink,
   cache: new InMemoryCache()
 });
-*/
-//Definiendo la URL
-const serverData = require('../server');
-let baseUrlFeedMs = `http://${serverData.feed_url}/${serverData.feed_entryPoint}`;
-let baseUrlLabelsMs = `http://${serverData.labels_url}/${serverData.labels_entryPoint}`;
-/*
+
 module.exports = {
     client
 }
-*/

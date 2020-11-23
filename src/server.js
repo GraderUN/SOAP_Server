0a40c30892bb9c32811e68b092ce326c287ebefa
saduquebe <saduquebe@unal.edu.co>
2020-11-22 20:08:01 -0500

    final changes :wheelchair:

--- src/server.js
+++ src/server.js
@@ -1,4 +1,6 @@
 module.exports = {
     //Data server soap
-    soap_server_port: process.env.SOAP_SERVER_PORT || "8000"
+    soap_server_port: process.env.SOAP_SERVER_PORT || "8000",
+    rest_server_port: process.env.REST_SERVER_PORT || "9000"
+
 };
